11cfeee2af4c7ea58c0fd29d97300b3ce8ea23b9
Raizer58 <Raizer9058w@gmail.com>
2020-02-27 06:45:21 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    n = String(Math.abs(n)).split("");
   
-}
+    const midNumber = n.splice(Math.floor(n.length)/2,1);
+  
+    n.reverse().splice(n.length/2,0,midNumber);
+  
+    return Number(n.join(""))
+  }
